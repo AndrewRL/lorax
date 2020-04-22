e10e97cc9a4e91d457db28029429e443fd36e897
Andrew Laird <alaird4@gmail.com>
2020-04-22 11:22:34 -0700

    Removed old bar chart file.

--- bar_chart_with_monthly_averages.py
+++ bar_chart_with_monthly_averages.py
@@ -9,13 +9,14 @@ from datetime import datetime
 #Configuration Options
 PERIODIC_AVERAGES_FILE_PATH = 'data/monthly_averages.xlsx'
 PERIODIC_AVERAGES_SKIPROWS = []
+PERIODIC_AVERAGE_DATA_COLUMN_NAME = 'Average Precip (mm)'
 BAR_DATA_FILE_PATH = 'data/weatherstndata_filtered.xlsx'
 BAR_DATA_SKIPROWS = [1]
 BAR_DATA_DATE_COLUMN_NAME = 'DateTime'
 BAR_DATA_DATA_COLUMN_NAME = 'PG_1hr'
 PLOT_TITLE = 'Monthly Rainfall Averages with Daily Rainfall Bars for 2019'
-PLOT_XLABEL =
-PLOT_YLABEL =
+PLOT_XLABEL = 'Month'
+PLOT_YLABEL = 'Precipitation (mm)'
 OUTPUT_FILE_PATH = 'data/weatherstndata_chart.png'
 
 # Read in the hourly bars
@@ -28,11 +29,8 @@ monthly_averages = pd.read_excel(PERIODIC_AVERAGES_FILE_PATH, header=0, skiprows
 fig, ax = plt.subplots()
 ax.bar(BAR_DATA_DATE_COLUMN_NAME, BAR_DATA_DATA_COLUMN_NAME, data=bar_data, color='black')
 
-month_starts = [datetime(2019, month, 1, 0, 0, 0) for month in range(1, 13)]
+month_starts = [datetime(2019, month, 1, 12, 0, 0) for month in range(1, 13)]
 month_starts.append(datetime(2020, 1, 1, 0, 0, 0))
-
-for index, average in enumerate(monthly_averages['Average Precip (mm)']):
-    plt.plot([month_starts[index], month_starts[index+1]], [average, average], color='crimson')
 
 months = mdates.MonthLocator()
 months_fmt = mdates.DateFormatter('%b %Y')
@@ -43,10 +41,15 @@ datemin = pd.to_datetime(bar_data[BAR_DATA_DATE_COLUMN_NAME].iloc[0], format='%Y
 datemax = pd.to_datetime(bar_data[BAR_DATA_DATE_COLUMN_NAME].iloc[-1], format='%Y-%b-%d %H:%M:%S')
 ax.set_xlim(datemin, datemax)
 
-plt.bar(bar_data['DateTime'], bar_data['PG_1hr'])
-plt.title()
-plt.xlabel('Month')
-plt.ylabel('Precipitation (mm)')
+locs, _ = plt.xticks()
+print(locs)
+for index, average in enumerate(monthly_averages[PERIODIC_AVERAGE_DATA_COLUMN_NAME]):
+    plt.plot([month_starts[index], month_starts[index+1]], [average, average], color='crimson')
+
+plt.bar(bar_data[BAR_DATA_DATE_COLUMN_NAME], bar_data[BAR_DATA_DATA_COLUMN_NAME])
+plt.title(PLOT_TITLE)
+plt.xlabel(PLOT_XLABEL)
+plt.ylabel(PLOT_YLABEL)
 plt.grid(True)
 
 fig.autofmt_xdate()
